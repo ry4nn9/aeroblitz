50e0898f345dd8d479fae1f6d58a75f759d2ad5d
ry4nn9 <109567010+ry4nn9@users.noreply.github.com>
2022-11-30 00:10:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import jetC
 import mslC
 import random
 
+################################################
+# first-person view (bonus feature)
+################################################
+
 def appStarted(app):
     app.score = 0
     app.planeFullHealth = 100
@@ -34,25 +38,254 @@ def drawHealthBar(app, canvas):
         x1 += 1/5*app.width
     # health percentage
 
-def redrawAll(app, canvas):
+def firstPerson_redrawAll(app, canvas):
     drawJetFirstPerson(app, canvas)
     drawHealthBar(app, canvas)
 
-def keyPressed(app, event):
+def firstPerson_keyPressed(app, event):
     pass
 
-def mousePressed(app, event):
+def firstPerson_mousePressed(app, event):
     pass
 
-# bird's eye view
+################################################
+# home screen
+################################################
+
+def drawHomeScreen(app, canvas):
+    cx = app.width//2
+    cy = app.height//2
+    canvas.create_rectangle(0, 0, app.width, app.height, fill = "black")
+    canvas.create_rectangle(cx-300, cy-350, cx+300, cy+350, fill = 'black', 
+                            outline = 'red')
+    canvas.create_text(cx, cy-200, text = 'missileAirDefense', 
+                        font = 'Courier 50 bold', fill = 'red')
+
+def start_redrawAll(app, canvas):
+    drawHomeScreen(app, canvas)
+    for enemy in app.enemyJets:
+        drawEnemyJet(app, canvas, enemy[0], enemy[1], 1)
+    for missile in app.enemyMissiles:
+        missile.redraw(app, canvas)
+
+    # draw "choose your fighter"
+    canvas.create_text(app.width//2, app.height-200, text = 'Choose Your Fighter',
+                        font = 'Courier 30 bold', fill = 'maroon')
+    # draw selection of jets 
+    app.Hawk.redraw(app, canvas)
+    app.Falcon.redraw(app, canvas)
+    app.Thunderbolt.redraw(app, canvas)
+
+def start_mousePressed(app, event):
+    # when you click on figter jet, screen will show strengths and weaknesses of jet
+    if app.width//2-80 <= event.x <= app.width//2+80:
+        if (4*app.height/5-10) <= event.y <= (app.height-30):
+            app.mode = 'hawkInfo'
+    if app.width//3-80 <= event.x <= app.width//3+80:
+        if (4*app.height/5-10) <= event.y <= (app.height-30):
+            app.mode = 'falconInfo'
+    if 2*app.width//3-80 <= event.x <= 2*app.width//3+80:
+        if (4*app.height/5-10) <= event.y <= (app.height-30):
+            app.mode = 'thunderboltInfo'
+
+def start_mouseMoved(app, event):
+    # when you hover over fighter the outline of jet will turn white
+    if app.width//2-80 <= event.x <= app.width//2+80:
+        if (4*app.height/5-10) <= event.y <= (app.height-30):
+            app.Hawk.color = 'white'
+    else:
+        app.Hawk.color = 'black'
+    if app.width//3-80 <= event.x <= app.width//3+80:
+        if (4*app.height/5-10) <= event.y <= (app.height-30):
+            app.Falcon.color = 'white'
+    else:
+        app.Falcon.color = 'black'
+    if 2*app.width//3-80 <= event.x <= 2*app.width//3+80:
+        if (4*app.height/5-10) <= event.y <= (app.height-30):
+            app.Thunderbolt.color = 'white'
+    else:
+        app.Thunderbolt.color = 'black'
+
+def start_timerFired(app):
+    app.timePassed += 10*app.timerDelay
+    
+    # spawn jets
+    enemySpawnX0 = random.randint(50, app.width//2-350)
+    enemySpawnX1 = random.randint(app.width//2+350, app.width-100)
+    L = [enemySpawnX0, enemySpawnX1]
+    for x in L:
+        bound1 = x - 50
+        bound2 = x + 50
+        if len(app.enemyJets) <= 1:
+            if isNotOverlap(app, bound1, bound2):
+                app.enemyJetXCoord.add(x)
+                app.enemyJets.append([x, app.enemySpawnY0])
+        
+    # move jets
+    for coordinate in app.enemyJets:
+        coordinate[1] += 2
+        if coordinate[1] >= app.height-100:
+            app.enemyJets.remove(coordinate)
+            app.enemyJetXCoord.remove(coordinate[0])
+    
+    # add missiles to enemy jet
+    if app.timePassed % 1000 == 0:
+        addEnemyMissile(app)
+    
+    # shoot missiles
+    for missile in app.enemyMissiles:
+        missile.eq()
+
+
+################################################
+# jet information screen
+################################################
+def drawHealth(app, canvas, health):
+    canvas.create_rectangle(app.width//2-450, app.height//2+75, app.width//2+450, 
+                            app.height//2+65, fill = 'black', outline = 'black')
+    x0 = app.width//2-450
+    y0 = app.height//2+75
+    x1 = x0+7
+    y1 = app.height//2+65
+    for bar in range(health//2+1):
+        canvas.create_rectangle(x0, y0, x1, y1, fill = 'red', outline = 'black')
+        x0, x1 = x1, x0+7
+    canvas.create_text(300, y0-20, text = 'health', fill = 'white', 
+                        font = 'Courier 14 bold')
+
+def drawSpeed(app, canvas, speed):
+    canvas.create_rectangle(app.width//2-450, app.height//2+95, app.width//2+450, 
+                            app.height//2+105, fill = 'black', outline = 'black')
+    x0 = app.width//2-450
+    y0 = app.height//2+95
+    x1 = x0+17
+    y1 = app.height//2+105
+    for bar in range(speed//2+1):
+        canvas.create_rectangle(x0, y0, x1, y1, fill = 'red', outline = 'black')
+        x0, x1 = x1, x0+17
+    canvas.create_text(295, y0-10, text = 'speed', fill = 'white', 
+                        font = 'Courier 14 bold')
+
+# Hawk's Info
+###############################################
+def hawkInfo_redrawAll(app, canvas):
+    cx = app.width//2
+    cy = app.height//2
+    canvas.create_rectangle(0, 0, app.width, app.height,
+                            fill = 'black')
+    canvas.create_rectangle(cx-500, cy-150, cx+500, cy+150, fill = 'gray21',
+                            outline = 'medium sea green')
+    canvas.create_rectangle(cx-70, cy+200, cx+70, cy+250, fill = 'red')
+    canvas.create_text(cx, cy+225, text = 'Play', font = 'Courier 20 bold',
+                        fill = 'black')
+    colors = ['medium sea green', 'OliveDrab4', 'LightSteelBlue4', 'black', 'OliveDrab4',
+                 'gray16', 'gray16', 'OliveDrab4', 'gray55', 'gray55']
+    drawUserJet(app, canvas, app.width//2, app.height//2-100, colors, 'Hawk')
+    drawHealth(app, canvas, app.Hawk.health)
+    drawSpeed(app, canvas, app.Hawk.speed)
+
+def hawkInfo_keyPressed(app, event):
+    if event.key == 'Return':
+        appStarted(app)
+        app.userJet = app.jetSelection[0]
+        app.userJetFull = app.userJet.health
+        app.mode = 'gameMode'
+
+def hawkInfo_mousePressed(app, event):
+    cx = app.width//2
+    cy = app.height//2
+    if cx-70 <= event.x <= cx+70:
+        if cy+200 <= event.y <= cy+250:
+            appStarted(app)
+            app.userJet = app.jetSelection[0]
+            app.userJetFull = app.userJet.health
+            app.mode = 'gameMode'
+
+# Falcon's Info
+###############################################
+def falconInfo_redrawAll(app, canvas):
+    cx = app.width//2
+    cy = app.height//2
+    canvas.create_rectangle(0, 0, app.width, app.height,
+                            fill = 'black')
+    canvas.create_rectangle(cx-500, cy-150, cx+500, cy+150, fill = 'grey10',
+                            outline = 'GreenYellow')
+    canvas.create_rectangle(cx-70, cy+200, cx+70, cy+250, fill = 'red')
+    canvas.create_text(cx, cy+225, text = 'Play', font = 'Courier 20 bold',
+                        fill = 'black')
+    colors = ['bisque4', 'bisque4', 'GreenYellow', 'green', 'burlywood4', 
+            'burlywood4', 'burlywood4', 'burlywood4', 'coral4', 'coral4']
+    drawUserJet(app, canvas, app.width//2, app.height//2-100, colors, 'Falcon')
+    drawHealth(app, canvas, app.Falcon.health)
+    drawSpeed(app, canvas, app.Falcon.speed)
+
+def falconInfo_keyPressed(app, event):
+    if event.key == 'Return':
+        appStarted(app)
+        app.userJet = app.jetSelection[1]
+        app.userJetFull = app.userJet.health
+        app.mode = 'gameMode'
+
+def falconInfo_mousePressed(app, event):
+    cx = app.width//2
+    cy = app.height//2
+    if cx-70 <= event.x <= cx+70:
+        if cy+200 <= event.y <= cy+250:
+            appStarted(app)
+            app.userJet = app.jetSelection[1]
+            app.userJetFull = app.userJet.health
+            app.mode = 'gameMode'
+
+# Thunderbolt's Info
+###############################################
+def thunderboltInfo_redrawAll(app, canvas):
+    cx = app.width//2
+    cy = app.height//2
+    canvas.create_rectangle(0, 0, app.width, app.height,
+                            fill = 'black')
+    canvas.create_rectangle(cx-500, cy-150, cx+500, cy+150, fill = 'grey15',
+                            outline = 'medium slate blue')
+    canvas.create_rectangle(cx-70, cy+200, cx+70, cy+250, fill = 'red')
+    canvas.create_text(cx, cy+225, text = 'Play', font = 'Courier 20 bold',
+                        fill = 'black')
+    colors = ['grey61', 'grey30', 'medium slate blue', 'purple', 'grey35', 
+            'grey35', 'grey35', 'grey35', 'MediumPurple4', 'MediumPurple4']
+    drawUserJet(app, canvas, app.width//2, app.height//2-100, colors, 'Thunderbolt')
+    drawHealth(app, canvas, app.Thunderbolt.health)
+    drawSpeed(app, canvas, app.Thunderbolt.speed)
+
+def thunderboltInfo_keyPressed(app, event):
+    if event.key == 'Return':
+        appStarted(app)
+        app.userJet = app.jetSelection[2]
+        app.userJetFull = app.userJet.health
+        app.mode = 'gameMode'
+
+def thunderboltInfo_mousePressed(app, event):
+    cx = app.width//2
+    cy = app.height//2
+    if cx-70 <= event.x <= cx+70:
+        if cy+200 <= event.y <= cy+250:
+            appStarted(app)
+            app.userJet = app.jetSelection[2]
+            app.userJetFull = app.userJet.health
+            app.mode = 'gameMode'
+
+################################################
+# game play (bird-eye view)
+################################################
 
 def appStarted(app):
+    # starting screen
+    app.mode = 'start'
+
     # gameplay features/information
     app.score = 0
     app.gameOver = False
     app.timePassed = 0
     app.difficultyTracker = 0
-    app.enemyJetCapacity = 0
+    # app.enemyJetCapacity = 0
+    app.enemyJetCapacity = 2
     app.timerDelay = 10
     app.pause = False
     app.mslPerSec = 2000 # increase fire rate by 0.04s every 10 targets down
@@ -76,19 +309,22 @@ def appStarted(app):
     # app.image1 = 
 
     # selection of Jets (for user)
-    Hawk = jetC.Hawk(300, 100, app.UserX, app.UserY)
-    Falcon = jetC.Falcon(400, 75, app.UserX, app.UserY)
-    Thunderbolt = jetC.Thunderbolt(250, 150, app.UserX, app.UserY)
+    Hawk = jetC.Hawk(400, 100, app.UserX, app.UserY, 'black')
+    Falcon = jetC.Falcon(500, 75, app.UserX, app.UserY, 'black')
+    Thunderbolt = jetC.Thunderbolt(250, 200, app.UserX, app.UserY, 'black')
     app.jetSelection = [Hawk, Falcon, Thunderbolt]
-    app.userJet = random.choice(app.jetSelection)
-    app.userJetFull = app.userJet.health
 
     # types of missiles (for enemy)
     # app.enemyMissileTypes = ['cubic']
     app.enemyMissileTypes = ['linear', 'sin', 'para1', 'para2', 'cubic']
 
+    # home screen selection
+    app.Hawk = jetC.Hawk(350, 100, app.width//2, app.height-(1/5*app.height), 'black')
+    app.Falcon = jetC.Falcon(500, 75, app.width//3, app.height-(1/5*app.height), 'black')
+    app.Thunderbolt = jetC.Thunderbolt(250, 200, 2*app.width//3, app.height-(1/5*app.height), 'black')
 
-def mousePressed(app, event):
+
+def gameMode_mousePressed(app, event):
     if app.gameOver:
         return
     if app.pause:
@@ -99,7 +335,7 @@ def mousePressed(app, event):
     if 25 <= event.x <= 50 and 25 <= event.y <= 50:
         app.pause = not app.pause
             
-def keyPressed(app, event):
+def gameMode_keyPressed(app, event):
     if event.key == 'r':
         appStarted(app)
 
@@ -139,7 +375,7 @@ def keyPressed(app, event):
             app.userJet.health = 0
         
 
-def timerFired(app):
+def gameMode_timerFired(app):
     if app.userJet.health <= 0:
         app.gameOver = True
         return
@@ -157,8 +393,8 @@ def timerFired(app):
     if app.timePassed % 5 == 0:
         moveEnemyJet(app)
     # need to set general variable for addEnemyMissile (default = 1000)
-    if app.timePassed % app.mslPerSec == 0:
-    # if app.timePassed % 200 == 0: # test conditional
+    # if app.timePassed % app.mslPerSec == 0:
+    if app.timePassed % 250 == 0: # test conditional
         addEnemyMissile(app)
     # need to set general variable for shootEnemyMissile
     if app.timePassed % 10 == 0:
@@ -194,7 +430,6 @@ def drawHealthBar(app, canvas, health, divider):
     canvas.create_text(app.width-45, 42, text = f'{app.userJet.health}',
                         fill = 'red', font = 'Courier 30 bold')
 
-
 def drawPauseButton(app, canvas):
     canvas.create_image
     canvas.create_rectangle(25, 25, 50, 50, fill = 'navy blue', outline = 'royal blue',
@@ -205,7 +440,7 @@ def drawPauseButton(app, canvas):
 def drawPause(app, canvas):
     cx = app.width//2
     cy = app.height//2
-    canvas.create_rectangle(cx - 110, cy - 50, cx + 110, cy + 60, fill = 'black')
+    canvas.create_rectangle(cx - 210, cy - 100, cx + 210, cy + 110, fill = 'black')
     canvas.create_text(app.width//2, app.height//2, text = 'Game paused', 
                         font = 'Courier 30 bold', fill = 'red')
     canvas.create_text(app.width//2, app.height//2+25, text = 'Press "p" to unpause', fill = 'red',
@@ -236,6 +471,49 @@ def drawMissileCounter(app, canvas):
     canvas.create_text(app.width//2, 90, text = f'Enemy Fire Rate: 1 msls/{app.mslPerSec/1000}sec', fill = 'green',
                         font = 'Courier 15')
 
+def drawUserJet(app, canvas, x0, y0, colors, name):
+    # upper half of body
+    canvas.create_arc(x0 - 15, y0+70, x0 + 15, y0-110, 
+                        fill = colors[0], outline = 'black', extent = 180)
+     # lower half of body
+    canvas.create_arc(x0 - 15, y0-90, x0 + 15, y0+60, 
+                        fill = colors[1], outline = 'black', extent = -180)
+    canvas.create_rectangle(x0+15, y0-20, x0-15, y0-13, fill = 'black', 
+                            outline = 'black')
+    # pilot's head-up-display (HUD)
+    canvas.create_oval(x0 - 7, y0-5, x0 + 7, y0-100, fill = colors[2],
+                        outline = colors[3])
+     # right front wing
+    wingY = (y0-10+y0-30)//2     
+    canvas.create_polygon(x0 + 15, wingY, x0 + 50, wingY, x0 + 80, wingY + 20, 
+                        x0 + 10, wingY + 20, outline = 'black', 
+                        fill = colors[4])
+    # right tail
+    canvas.create_polygon(x0 + 10, wingY + 40, x0 + 30, wingY + 40, x0 + 50, 
+                        wingY + 55, x0 + 5, wingY + 55, outline = 'black', 
+                        fill = colors[5])
+    # left front wing
+    canvas.create_polygon(x0 - 15, wingY, x0 - 50, wingY, x0 - 80, wingY + 20, 
+                        x0 - 10, wingY + 20, outline = 'black', 
+                        fill = colors[6])
+    # left tail
+    canvas.create_polygon(x0 - 10, wingY + 40, x0 - 30, wingY + 40, x0 - 50, 
+                        wingY + 55, x0 - 5, wingY + 55, outline = 'black', 
+                        fill = colors[7])
+    # right missile shooter
+    mslCx = x0 + 7.5
+    canvas.create_rectangle(mslCx + 30, wingY - 10, mslCx + 20, wingY, 
+                            fill = colors[8], outline = 'black')
+    
+    # left missile shooter
+    mslCx1 = x0 - 7.5
+    canvas.create_rectangle(mslCx1 - 30, wingY - 10, mslCx1 - 20, wingY, 
+                            fill = colors[9], outline = 'black')
+
+    # name tag
+    canvas.create_text(x0, y0+80, text = name, fill = 'white',
+                        font = 'Courier 25 bold')
+                            
 def drawEnemyJet(app, canvas, x0, y0, unit):
     # draw jet's body
     # x and y-coord in app.enemyJets are the center coordinates of enemy jets
@@ -398,7 +676,7 @@ def hitboxUserEnemyMissiles(app, userMissiles, enemyMissile):
         return hitboxUserEnemyMissiles(app, userMissiles[1:], enemyMissile)
 
 
-def redrawAll(app, canvas):
+def gameMode_redrawAll(app, canvas):
     # draw background first
     drawBackground(app, canvas)
     # draw icon for enemy jet capacity
